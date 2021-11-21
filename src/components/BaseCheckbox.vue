f5a73178d1dc9f4d0c1fb6f65489bc6ae780b0bd
Nathan Richards <nathinho@outlook.com>
2021-11-21 14:48:52 +0000

    Base files pre axios integration

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input
+          :checked="modelValue"
+          @change="$emit('update:modelValue', $event.target.checked)"
+          type="checkbox"
+          class="field"
+        />
+        <label v-if="label">{{ label }}</label>
+      </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
---
+++ src/components/BaseRadioGroup.vue
@@ -0,0 +1,42 @@
+<template>
+    <component
+        v-for="option in options"
+        :key="option.value"
+        :is="vertical ? 'div' : 'span'">
+        <BaseRadio
+            :label="option.label"
+            :value="option.value"
+            :name="name"
+            :modelValue="modelValue"
+            @update:modelValue="$emit('update:modelValue', $event)"/>
+    </component>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: [String, Number, Boolean],
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    vertical: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  span {
+    margin-right:1rem;
+  }
+</style>
